7bd0dff25c3c7fec1a7a4a4f9ae61fe6e388f547
Aman Shah <aman.shah@matillion.com>
2024-03-10 15:57:11 +0000

    create a basic endpoint with Gin

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 func main() {
-	// TODO
+	server := gin.Default()
+	server.GET("/events", getEvents)
+	server.Run(":8080")
+}
+
+// A handler method receives a pointer to the Gin Context
+func getEvents(context *gin.Context) {
+	// The context can be used to send a response.
+	// JSON responses require a status code and an object to be converted to JSON (a map in this case).
+	context.JSON(http.StatusOK, gin.H{"message": "Hello"})
 }
