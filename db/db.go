3d614dad6db74beb8ccb3b2eb2b9c501d309b26c
Aman Shah <aman.shah@matillion.com>
2024-03-12 13:30:56 +0000

    bugfix

--- db/db.go
+++ db/db.go
@@ -5,8 +5,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var DB *sql.DB
-
 func InitDB() {
 	DB, err := sql.Open("sqlite3", "api.db")
 	if err != nil {
@@ -16,20 +14,20 @@ func InitDB() {
 	DB.SetMaxOpenConns(10)
 	DB.SetMaxIdleConns(5)
 
-	createTables()
+	createTables(DB)
 }
 
-func createTables() {
+func createTables(DB *sql.DB) {
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
-	    id INTEGER PRIMARY KEY AUTOINCREMENT,
-	    name TEXT NOT NULL,
-		description TEXT NOT NULL,
-	 	location TEXT NOT NULL,
-	    dataTime DATETIME NOT NULL,
-	    user_id INTEGER
-	)
-	`
+	    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	    "event_name" TEXT NOT NULL,
+		"description" TEXT NOT NULL,
+	 	"location" TEXT NOT NULL,
+	    "dataTime" DATETIME NOT NULL,
+	    "user_id" INTEGER
+	);`
+
 	_, err := DB.Exec(createEventsTable)
 	if err != nil {
 		panic("Could not create events table. ")
