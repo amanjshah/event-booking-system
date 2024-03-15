8603aaa79a07138b98a23d72817e0dad0462334d
Aman Shah <aman.shah@matillion.com>
2024-03-15 13:15:06 +0000

    save to the DB rather than to the events local dynamic list

--- models/event.go
+++ models/event.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"github.com/amanjshah/event-booking-system/db"
+	"time"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -14,8 +17,25 @@ type Event struct {
 // Save in-memory til db is introduced
 var events = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e Event) Save() error {
+	// Note to self: always execute such queries like this (inject values via Exec method rather than adding values into the query string manually).
+	// Protects against SQL injection attacks.
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES(?,?,?,?,?)
+	`
+	statement, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+	result, err := statement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
 func GetAllEvents() []Event {
