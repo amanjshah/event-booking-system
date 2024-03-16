84be7c56cb38b7b1ceaf10bed24bf4f4e8b99702
Aman Shah <aman.j.shah@gmail.com>
2024-03-16 17:46:30 +0000

    add a users table and an endpoint that allows users to signup

---
+++ routes/users.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"github.com/amanjshah/event-booking-system/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to save user. Please retry. "})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User Created"})
+}
