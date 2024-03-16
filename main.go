2d751758e4acc0df3cca4236f738761efd0e6dd0
Aman Shah <aman.j.shah@gmail.com>
2024-03-16 15:09:19 +0000

    add endpoint to get event by ID

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"github.com/amanjshah/event-booking-system/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
 	server.GET("/events", getEvents)
+	server.GET("/events/:eventId", getEvent)
 	server.POST("/events", createEvent)
 	server.Run(":8080")
 }
@@ -25,6 +27,20 @@ func getEvents(context *gin.Context) {
 		return
 	}
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("eventId"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to fetch event. Please retry."})
+		return
+	}
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
