7ce43a0302198e1d7caffef2cfda5107a0260749
Aman Shah <aman.j.shah@gmail.com>
2024-03-17 18:06:16 +0000

    add registration table and a method on Event struct to register users for it

--- db/db.go
+++ db/db.go
@@ -27,7 +27,6 @@ func createTables() {
 	    "email" TEXT NOT NULL UNIQUE,
 	    "password" TEXT NOT NULL
 	);`
-
 	_, err := DB.Exec(createUsersTable)
 	if err != nil {
 		panic("Could not create users table. ")
@@ -42,10 +41,21 @@ func createTables() {
 	    "user_id" INTEGER,
 	    FOREIGN KEY(user_id) REFERENCES users(id)
 	);`
-
 	_, err = DB.Exec(createEventsTable)
 	if err != nil {
 		panic("Could not create events table. ")
 	}
 
+	createRegistrationsTable := `
+	CREATE TABLE IF NOT EXISTS registrations (
+	    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	    "event_id" INTEGER,
+	    "user_id" INTEGER,
+	    FOREIGN KEY(event_id) REFERENCES events(id),
+	    FOREIGN KEY(user_id) REFERENCES users(id)
+	);`
+	_, err = DB.Exec(createRegistrationsTable)
+	if err != nil {
+		panic("Could not create registrations table. ")
+	}
 }
